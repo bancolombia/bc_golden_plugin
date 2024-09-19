b332ecaf4acebb5fe3121b97d1b4139f0a49d0ed
Juan Campuzano <jcampuza@bancolombia.com.co>
2024-09-19 12:52:45 -0500

    tests: add more tests for golden testing tools

--- test/src/golden_testing_tools_test.dart
+++ test/src/golden_testing_tools_test.dart
@@ -1,9 +1,10 @@
+import 'package:bc_golden_plugin/bc_golden_plugin.dart';
 import 'package:bc_golden_plugin/src/helpers/window_size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:bc_golden_plugin/src/golden_testing_tools.dart';
 
 void main() {
+  int counter = 0;
   test('bcCustomWindowConfigData should return a WindowConfigData object', () {
     final configData = bcCustomWindowConfigData(
       name: 'Custom Config',
@@ -17,5 +18,17 @@ void main() {
     final iconData = IconData(0xe900, fontFamily: 'BdsFunctionalIcons');
     final convertedIcon = iconData.convertToGolden();
     expect(convertedIcon, isA<IconData>());
+  });
+
+  bcGoldenTest(
+    'Test with real shadows',
+    (tester) async {
+      counter++;
+    },
+    shouldUseRealShadows: true,
+  );
+
+  test('bcGoldenTest should run test with real shadows', () async {
+    expect(counter, 1);
   });
 }
